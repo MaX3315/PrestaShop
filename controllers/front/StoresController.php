4790583288c30688532ef7c614144fc4656a5115
MaX3315 <olivier@lbab.fr>
2017-03-28 16:16:19 +0200

    Update StoresController.php
    FO : using a google map API key for the store map.

--- controllers/front/StoresController.php
+++ controllers/front/StoresController.php
@@ -286,7 +286,8 @@ class StoresControllerCore extends FrontController
 
         if (!Configuration::get('PS_STORES_SIMPLIFIED')) {
             $default_country = new Country((int)Tools::getCountry());
-            $this->addJS('http'.((Configuration::get('PS_SSL_ENABLED') && Configuration::get('PS_SSL_ENABLED_EVERYWHERE')) ? 's' : '').'://maps.google.com/maps/api/js?region='.substr($default_country->iso_code, 0, 2));
+	    $google_map_api_key = Configuration::get('PS_GOOGLE_MAP_API_KEY');
+            $this->addJS('http'.((Configuration::get('PS_SSL_ENABLED') && Configuration::get('PS_SSL_ENABLED_EVERYWHERE')) ? 's' : '').'://maps.google.com/maps/api/js?'.($google_map_api_key !== false ?'key='.$google_map_api_key.'&':'').'region='.substr($default_country->iso_code, 0, 2));
             $this->addJS(_THEME_JS_DIR_.'stores.js');
         }
     }
